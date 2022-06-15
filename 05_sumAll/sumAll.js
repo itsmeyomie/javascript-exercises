25abceb4d7f431cf9beba316456d00479f9f9534
Hamisi Amani <40064353+itsmeyomie@users.noreply.github.com>
2022-06-15 13:50:38 +0300

    Update sumAll.js
    added a couple of files to make the code readable

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+let i=x;
+let sum=0;
+while(i<y){
+  i++;
+sum +=i;
+}
 
+console.log(sum+1);
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
